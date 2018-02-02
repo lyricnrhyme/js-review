183fe5e73388f230e8eae7399bbf514a6d0559c6
Jamie Yamashita <jamieyamashita@elizabeths-ipad.harris-agency.com>
2018-02-01 20:25:45 -1000

    Completed Exercise 12

--- exercises.js
+++ exercises.js
@@ -116,6 +116,15 @@ console.log(numValue);
 
  console log the object*/
 
+var hotel = {
+  name: "Hilton",
+  rooms: 200,
+  pool: true,
+  checkIn: null
+};
+
+console.log(hotel);
+
 /*13.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
